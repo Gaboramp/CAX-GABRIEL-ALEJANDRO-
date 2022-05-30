b46dbb15b1f0ea1f011a218f2f2e9e710bd6e468
Gaboramp <104370738+Gaboramp@users.noreply.github.com>
2022-05-30 09:10:13 -0500

    Add files via upload

---
+++ 1/Rep.py
@@ -0,0 +1,144 @@
+### My Life ###
+
+print('Welcome to the game MY LIFE ')
+input()
+
+
+Name=(input('select your calacter:a) Gabriel b) Iker c) Daniel '))
+input()
+
+if Name=="Gabriel":
+  print("Hi" ,Name ,"click enter to start your life")
+  input()
+  print('Your caracteristics are: Singing and going to parties all days ')
+  input()
+  All=(input("Okey it's alredy night and you have to decide if you go to singing Lessons or go to party,a)Lessons or b)Party "))
+  
+  if All=="Lessons":
+    input()
+    print('In your singing class the teacher tells you to keep going and that you have talent and you can have a future')
+    
+    input()
+    print('(As you now since you were little you were good at singing)')
+    
+    input()
+    print('After several classes the teacher told you that you are going to participate in a contest ')
+    
+    input()
+    print('You accept to participate in the contest ')
+    print('at the beginning of the contest it gave you nerves but at the end...')
+    
+    input()
+  
+    print('YOU WIN')
+    input()
+    print('At the end of the contest a celebrity invites you to a party that night')
+   
+    input()
+    print('And you accept')
+   
+    input()
+    print('In that party there were people smoking and using drugs, and they ask you if you want to try it')
+   
+    input()
+    print('You do not accept to try it and you tell them no because you can get to see ')
+    input()
+    print('The next day...')
+   
+    input()
+    print('Your teacher tells you that he can no longer teach you more than what you already know and that you should go with someone more professional')
+ 
+    input()
+    print('You found a very good teacher but he lives in another city, and you decide that you are going to move out')
+    input()
+    print('in the new city you barely have enough to pay for the apartment so you have a problem')
+    input()
+    print('To solve the problem you have to decide if you should leave school and look for a job or stay in school(Click enter)')
+    input()
+    Mon=(input('What are you going to choose a)Work or b)School')) 
+    if Mon=="Work":
+      print('Since you decided to quit school, you got a job in the morning')
+      print('and in the afternoons you have your singing classes')
+      input()
+      print('After many classes with your new teacher, he asks you if you plan to live from music?')
+      input()
+      print('and you say yes')
+      input()
+      print('Then since he is known he starts introducing you to famous people')
+      input()
+      print('Your teacher asked you what you were going to nickname yourself ')
+      input()
+      print('And you told him that your nickname was going to be bad dogg')
+      input()
+      print('And your teacher told you that you should start creating songs and that he would help you with the production ')
+      input()
+      print('When you finish writing the song your teacher helped you with the tools to record the song ')
+      input()
+      print('Seeing that you had talent, he offered you a deal and he was going to hire you a manager and technicians')
+      print('Now the technicians help you upload your song to music platforms')
+    if Mon=="School":
+      print('Because you decide to stay in school,you began to have more economic problems')
+      input()
+    
+      print('and you no longer have enough to pay the teacher')
+      input()
+      print('After a month you no longer had enough money and you had to return to your city')
+      input()
+     
+      print('When you finish the school in your country ')
+      
+  if All=="Party":
+    print('At the party you got drunk, but those who made the party liked you,that was the popular group ')
+    input()
+    print('After some parties and meetings coinciding, you started to be friend with some people of the popular  group')
+    input()
+    print('Now people from the popular group invite you to parties')
+    input()
+    print('After several parties together, they invite you to be part of their group')
+    
+    input()
+    print('And you ACCEPT')
+    input()
+    print('At a party with them you meet a very cute girl,and you invite her on a date')
+   
+    input()
+    print('After several dates with her, you ask her if she wants to be your girlfriend, and she tells you...')
+    input()
+    print('YES')
+    input()
+    
+    print('In one party that  you went alone with your friends ')
+    input()
+    print('Your friends already trust on you they offered you drugs')
+    input()
+    print('And you accept')
+    input()
+   
+    print('after several times trying drugs at parties or alone.Your girlfriend finds out')
+   
+    print('and she confronts you and tells you that you should get help or she breaks up with you(CLICK ENTER)')
+    input()
+    Hob=(input('Now you must choose: a)Help or b)Break')) 
+    if Hob=="Help":
+      print('For help you went to a psychologist')
+      input()
+      print('On your first appointment with the psychologist, she asked you a lot of questions and she scheduled your second appointment for tomorrow. ')
+      input()
+    
+      print('On your second date the psychologist told you that you should leave your group of friends')
+      input()
+      print('You think that was a bad idea but you did it with the support of your girlfriend')
+      input()
+      print('After several appointments with the psychologist, she tells you that you should look for a job.')
+      input()
+   
+      print('After your appointments with the psychologist, you talk to your girlfriend about everything and she tells you that you should choose a job.')
+      input()
+      Job=(input('Which job will you choose?a)assistant or b)animator')) 
+      if Hob=="Break":
+        print('Your girlfriend, seeing that you did not decide to seek help, she break up with you')
+        input()
+   
+        print('You keep going out to parties with your friends')
+        input()
+        print('But you remember your girlfriend and start spending all your money on drugs and parties')
