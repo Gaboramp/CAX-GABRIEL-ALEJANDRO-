fc626fa7ef2ad6df8abed9834969cc88bd57958b
Gaboramp <104370738+Gaboramp@users.noreply.github.com>
2022-06-05 20:31:14 -0500

    Add files via upload

---
+++ 1/10:04 class code constants .py
@@ -0,0 +1,22 @@
+#helo there
+
+x=10
+
+jamon=10
+
+justo=2345
+
+excusa='puedo ir al bano?'
+
+print(x)
+print(jamon)
+print(excusa)
+print(x)
+print(jamon)
+print(excusa)
+
+c0m0st4an=20
+print(c0m0st4an)
+print('el precio del jamon es:', jamon)
+
+
---
+++ 1/27:09Hola mundo .py
@@ -0,0 +1,5 @@
+print('Holamundo')
+print('My name is Gabriel')
+print('I am in the grate of 9 C')
+print('My favote food is:Sushi')
+print('And i practice Box')
